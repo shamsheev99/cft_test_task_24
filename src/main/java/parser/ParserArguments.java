7703f44e559e018b825b05795dbef34ea5b9280c
Шамшеев Павел Александрович <PAShamsheev@sberbank.ru>
2024-02-08 17:57:07 +0700

    refactoring

--- src/main/java/parser/ParserArguments.java
+++ src/main/java/parser/ParserArguments.java
@@ -1,5 +1,6 @@
 package parser;
 
+import org.apache.commons.cli.*;
 import confighandlers.DefaultConfigBuilder;
 
 import java.util.HashMap;
@@ -7,7 +8,14 @@ import java.util.HashMap;
 
 public class ParserArguments {
     private HashMap<Character, Object> arguments;
+
     public ParserArguments(String[] args) {
         DefaultConfigBuilder.CreateConfig.create();
+        parse(args);
     }
+
+    private void parse(String[] args) {
+        CommandLineParser parser = new DefaultParser();
+    }
+
 }
